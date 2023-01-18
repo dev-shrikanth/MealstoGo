dfbee0b0c5ade13f0cd892a9e8a591dd3b36b3ea
dev-shrikanth <dev@shrikanth.net>
2023-01-18 08:19:54 -0800

    Added navigation and additional blank screens

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import { Searchbar } from 'react-native-paper';
-import { StatusBar, FlatList, SafeAreaView } from 'react-native';
+import { FlatList } from 'react-native';
 import styled from 'styled-components/native';
-
+import { SafeArea } from '../../../utils/safe-area.component';
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled.View`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-`;
+// const RestaurantListContainer = styled.View`
+//   flex: 1;
+//   padding: ${(props) => props.theme.space[3]};
+// `;
 
 export const RestaurantsScreen = () => (
   <SafeArea>
