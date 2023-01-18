aee4dd41ed6e09f7139e2a093f4a73238ef5501d
dev-shrikanth <dev@shrikanth.net>
2023-01-18 15:46:13 -0800

    Loading Mockdata with ActivityIndicator

--- App.js
+++ App.js
@@ -11,7 +11,8 @@ import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 
 import { theme } from './src/infrastructure/theme';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
-import { SafeArea } from './src/utils/safe-area.component';
+import { SafeArea } from './src/components/utility/safe-area.component';
+import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context';
 
 const Tab = createBottomTabNavigator();
 
@@ -61,28 +62,30 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <Tab.Navigator
-            screenOptions={createScreenOptions}
-            tabBarOptions={{
-              activeTintColor: 'tomato',
-              inactiveTintColor: 'gray',
-            }}
-          >
-            <Tab.Screen
-              name='Restaurants'
-              component={RestaurantsScreen}
-            />
-            <Tab.Screen
-              name='Map'
-              component={Map}
-            />
-            <Tab.Screen
-              name='Settings'
-              component={Settings}
-            />
-          </Tab.Navigator>
-        </NavigationContainer>
+        <RestaurantsContextProvider>
+          <NavigationContainer>
+            <Tab.Navigator
+              screenOptions={createScreenOptions}
+              tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+              }}
+            >
+              <Tab.Screen
+                name='Restaurants'
+                component={RestaurantsScreen}
+              />
+              <Tab.Screen
+                name='Map'
+                component={Map}
+              />
+              <Tab.Screen
+                name='Settings'
+                component={Settings}
+              />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </RestaurantsContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style='auto' />
     </>
